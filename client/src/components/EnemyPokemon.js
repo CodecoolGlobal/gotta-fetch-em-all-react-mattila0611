8dffa3c50508a3567fab4145cd87d657bf1978ad
mattila0611 <mattila0611@gmail.com>
2023-04-13 09:40:57 +0200

    add styling to the location, random selector and countdown pages

--- client/src/components/EnemyPokemon.js
+++ client/src/components/EnemyPokemon.js
@@ -4,11 +4,13 @@ function EnemyPokemon(props) {
     return (
         <div className="enemyPokemon">
 
-            <h1>Wild {enemyPokemon.name.toUpperCase()} appeared!</h1>
+            <p>Wild {enemyPokemon.name.toUpperCase()} appeared!</p>
             <img src={enemyPokemon.sprites.front_default} alt="" />
-            <p className="enemyPokemonStat">HP: {enemyPokemon.stats[0].base_stat}</p>
-            <p className="enemyPokemonStat">Attack: {enemyPokemon.stats[1].base_stat}</p>
-            <p className="enemyPokemonStat">Defense: {enemyPokemon.stats[2].base_stat}</p>
+            <div className='enemyPokemonDetails'>
+                <p className="enemyPokemonStat">HP: {enemyPokemon.stats[0].base_stat}</p>
+                <p className="enemyPokemonStat">Attack: {enemyPokemon.stats[1].base_stat}</p>
+                <p className="enemyPokemonStat">Defense: {enemyPokemon.stats[2].base_stat}</p>
+            </div>
         </div>
     )
 }
